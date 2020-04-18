97f8fd6a994704b444a74ff389d9b4af6fd3e6b9
Daniil S <damasterrr@users.noreply.github.com>
2020-04-18 21:53:09 +0200

    started solving
    Maybe we will need the panda package

--- Aufgabe 5.py
+++ Aufgabe 5.py
@@ -3,9 +3,31 @@
 #A ist dabei ein gegebener Anfangsbestand, a die Zuwachsrate und I der Endbe- stand nach t Zeiteinheiten (z.B. Tagen). Schreiben Sie eine Funktion bestand(A, a, t), die das Ergebnis entsprechend der Gleichung berechnet und zuru ̈ckgibt (2 Punkte). Lo ̈sen Sie nun die folgenden Aufgaben:
 
 #a. Welchen Endbestand gibt es fu ̈r A = 1000 bei einer Verdoppelung alle zwei Tage nach zwei Wochen? Geben Sie die Parameter und das Ergebnis am Bildschirm aus.(1 Punkte)
-#b. Welchen Endzustand, wenn man die Verdoppelungsrate auf acht Tage re- duzieren kann? Ausgabe wie vorher.(1 Punkte)
+#b. Welchen Endzustand, wenn man die Verdoppelungsrate auf acht Tage reduzieren kann? Ausgabe wie vorher.(1 Punkte)
 #c. Nehmen Sie die Daten wie in Aufgabe b an, ermitteln Sie jetzt aber die Bestandswerte mithilfe einer Schleife fu ̈r jeden Tag und geben Sie sie in zwei Spalten (Tag und Bestand) am Bildschirm aus. (2 Punkte)
 #d. Verwenden Sie die Schleife von Aufgabec in einer neuenvFunktion bestandtab(A,T,t), wobei T die Verdoppelungsrate in Tagen ist. Das Ergebnis der Funktion soll eine Liste von Tupeln (Tag,Bestand) sein. Erzeugen Sie eine Tabelle fu ̈r 6 Wochen bei 500 Anfangsbestand und einer Verdoppelungsrate von 10 Tagen
 #und geben Sie diese wiederum aus. (3 Punkte)
-#Alle Ausgaben mu ̈ssen auf ganze Zahlen gerundet erfolgen; verwenden Sie dazu
-die interne Python-Funktion round(x). (insgesamt 9 Punkte)
+#Alle Ausgaben mu ̈ssen auf ganze Zahlen gerundet erfolgen; verwenden Sie dazu die interne Python-Funktion round(x). (insgesamt 9 Punkte)
+
+#erster Teil der Aufgabe
+def bestand(A, a, t):
+	ergebnis = A * a**t
+	return ergebnis
+
+#a.
+a_wert = bestand(1000,2,14)
+print(a_wert)
+
+#b.
+b_wert = bestand(1000,2,8)
+print(b_wert)
+
+#c.
+#pandas for DataFrame and visualisation possibility?
+
+#d.
+#neue Funktion mit Tupel. eher import pandas?
+
+
+
+	
