fb8f9c05572d0582bf65f2957b38b83808a3de0c
Daniil S <damasterrr@users.noreply.github.com>
2020-04-18 20:50:18 +0200

    started
    need to go to sleep and then try again, but idea is clear

--- Aufgabe 4.py
+++ Aufgabe 4.py
@@ -1,2 +1,14 @@
-#Die Summe der ersten Quadratzahlen kann wie folgt berechnet werden: Summe k=1 bis n [n(n + 1)(2n + 1)]/6
+#Die Summe der ersten Quadratzahlen kann wie folgt berechnet werden: 
+		#Summe k=1 bis n k^2 = [n(n + 1)(2n + 1)]/6 
 #Zeigen Sie mit einer Schleife fu ̈r n = 1, 2, 3, ..., dass die Formel zumindest fu ̈r die ersten 30 natu ̈rlichen Zahlen korrekt ist. Geben Sie in einer Tabelle n sowie das Ergebnis der linken und auch der rechten Seite am Bildschirm aus.(3 Punkte)
+
+
+k = 3
+qudr = k**2
+print(qudr)
+
+#n = int(30)
+#liste = list(range(1,31))
+#n = int(2)
+#sum = (n*(n+1)*(2*n+1))/6
+#print(sum)
