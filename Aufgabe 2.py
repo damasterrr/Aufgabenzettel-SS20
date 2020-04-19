2ce9a51c01d5f6084cd3581f74f12281a11bf267
Daniil S <damasterrr@users.noreply.github.com>
2020-04-19 15:09:19 +0200

    Aufgabe 2 solved
    Seems to be correct,
    bin(<int>) won't call

--- Aufgabe 2.py
+++ Aufgabe 2.py
@@ -1,8 +1,17 @@
 #Lassen Sie den Benutzer eine beliebige Zahl bis 100 einlesen und wandeln sie diese in bina ̈re und oktale Darstellung um. Fassen Sie nun diese Darstellung als 2 neue Dezimalzahlen auf und geben Sie diese aus. Fu ̈hren Sie nun mindestens 4 Ihnen bekannte bin ̈are Rechenoperationen mit diesen beiden Zahlen aus und geben Sie diese aus. (3 Punkte)
 eingabe = int(input('gib eine Zahl von 1 bis 100 ein: '))
-print('deine Zahl in binärer Darstellung: ',bin(eingabe))
-print('deine Zahl in oktaler Darstellung: ',oct(eingabe))
+bin = bin(eingabe)
+oct = oct(eingabe)
+print('deine Zahl in binärer Darstellung: ', bin[2:])
+print('deine Zahl in oktaler Darstellung: ', oct[2:])
 
-#Die beiden Argumente wieder in die ursprungliche Form umwandeln
+#binäre Rechneoperationen
+b = int(bin[2:])
+o = int(oct[2:])
+addition = b + o
+subtraktion = b - o
+multiplikation = b * o
+division = b / o
 
-print('deine eingegebene Zahl wieder: ',int(bin(eingabe), base = 2))
+#print(addition, subtraktion, multiplikation, division)
+
