e95ddc7657d9e2568f72bef92106277ab71a615a
Daniil S <damasterrr@users.noreply.github.com>
2020-04-19 21:18:40 +0200

    Aufgabe 4 solved

--- Aufgabe 4.py
+++ Aufgabe 4.py
@@ -2,13 +2,14 @@
 		#Summe k=1 bis n k^2 = [n(n + 1)(2n + 1)]/6 
 #Zeigen Sie mit einer Schleife fu ̈r n = 1, 2, 3, ..., dass die Formel zumindest fu ̈r die ersten 30 natu ̈rlichen Zahlen korrekt ist. Geben Sie in einer Tabelle n sowie das Ergebnis der linken und auch der rechten Seite am Bildschirm aus.(3 Punkte)
 
-
-k = 3
-qudr = k**2
-print(qudr)
-
-#n = int(30)
-#liste = list(range(1,31))
-#n = int(2)
-#sum = (n*(n+1)*(2*n+1))/6
-#print(sum)
+sum_1 = 0
+k = 1
+werte_n = range(1,31)
+for n in werte_n: 
+	sum_1 = sum_1 + k**2
+	k = k+1
+	sum_2 = (n*(n+1)*(2*n+1))/6
+	if n <= 13:
+		print(sum_1, sum_2, sep = 2*'\t') #weil die kleineren Zahlen kürzer sind
+	else:
+		print(sum_1, sum_2, sep = '\t')
